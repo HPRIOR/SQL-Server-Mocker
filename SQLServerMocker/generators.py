670e4a81dfb0b7d7c4738dd525fbd239ebc53286
HPRIOR <hprior02@dcs.bbk.ac.uk>
2020-06-18 18:09:10 +0100

    PopulateTable class finished

--- SQLServerMocker/generators.py
+++ SQLServerMocker/generators.py
@@ -1,2 +1,35 @@
 import random
 
+
+def get_gen_dict():
+    pass
+
+
+class Generator:
+    def next(self):
+        pass
+
+
+class GenerateID(Generator):
+    def __init__(self, col_name):
+        self.col_name = col_name
+        self.iter = 0
+
+    def next(self):
+        r_val = f"'{self.col_name}_{self.iter}'"
+        self.iter += 1
+        return r_val
+
+
+class ReferenceDict:
+    """
+    contains dictionary of reference generators:
+    key = column reference which has already been created
+    value = values which have been created for that column reference
+    """
+
+
+class FKValueGenerator:
+    """
+    gives random values from the reference Dict
+    """
--- SQLServerMocker/populate_tables.py
+++ SQLServerMocker/populate_tables.py
@@ -1,7 +1,38 @@
 from SQLServerMocker.get_database import *
+from SQLServerMocker.generators import *
+
 
 class PopulateTable:
-    def __init__(self, db: DataBase, gen_dict):
-        self.db = db
-        self.gen_dict
+    """
+    generates string to populate table
+    """
+    def __init__(self, db_name: str, table: Table, num_rows: int, generators: [Generator]):
+        self.table = table
+        self.num_rows = num_rows
+        self.generators = generators
+        self.db_name = db_name
 
+    def string_builder(self):
+        s = ""
+        s += f"INSERT into {self.db_name}.dbo.{self.table.name} ({', '.join([col.name for col in self.table.columns])})\n"
+        s += "VALUES\n"
+        for i in range(self.num_rows):
+            s += "("
+            for generator in self.generators:
+                s += generator.next()
+            if i < self.num_rows:
+                s += "), \n"
+            else:
+                s += ") \n"
+
+
+
+
+class Controller:
+    """
+    Coordinates generation of table values
+    checks table for fk/pk values, determines which generator is called by PopulateTable
+    """
+    def __init__(self, db: DataBase, nums_rows: [int]):
+        self.db = db
+        self.nums_rows = nums_rows
