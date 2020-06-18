0ab6325dcec91f15291ec684b4d4dce4bb8f6feb
HPRIOR <hprior02@dcs.bbk.ac.uk>
2020-06-18 15:09:36 +0100

    refactored create_tables.py with l-comp

--- SQLServerMocker/create_tables.py
+++ SQLServerMocker/create_tables.py
@@ -1,4 +1,4 @@
-from SQLServerMocker.get_database import CreateDataBase, DataBase, Table, get_json
+from SQLServerMocker.get_database import *
 
 
 class CreateTable:
@@ -10,46 +10,28 @@ class CreateTable:
         return f"CREATE TABLE {self.db.name}.dbo.{table.name} (\n"
 
     def column_string(self, table: Table) -> str:
-        cols = ""
-        for col in table.columns:
-            if col.not_null:
-                cols += f"{col.name} {col.data_type} NOT NULL,\n"
-            else:
-                cols += f"{col.name} {col.data_type},\n"
-        return cols
+        return "".join([f"{col.name} {col.data_type} NOT NULL,\n" if col.not_null
+                        else f"{col.name} {col.data_type}\n" for col in table.columns])
 
     def constraint_string(self, table: Table) -> str:
-        cols = ""
-        for col in table.columns:
-            if col.foreign_key != "None":
-                cols += f"FOREIGN KEY ({col.name}) REFERENCES {self.db.name}.dbo.{col.foreign_key} ({col.name}), \n"
-        return cols
+        return "".join([f"FOREIGN KEY ({col.name}) REFERENCES {self.db.name}.dbo.{col.foreign_key} ({col.name}), \n"
+                        for col in table.columns if col.foreign_key != "None"])
 
     def primary_key_string(self, table: Table) -> str:
-        s = ""
-        pk_list = [col.name for col in table.columns if col.primary_key]
-        for enum, key in enumerate(pk_list):
-            if enum < len(pk_list) - 1:
-                s += key + ", "
-            else:
-                s += key
-        return f"PRIMARY KEY ({s}) \n \n"
+        return f"PRIMARY KEY ({', '.join([col.name for col in table.columns if col.primary_key])}) \n \n"
 
-    def string_builder(self, table: Table) -> str:
+    def string_builder(self) -> str:
         s = ""
-        s += self.initial_string(table)
-        s += self.column_string(table)
-        s += self.constraint_string(table)
-        s += self.primary_key_string(table)
+        for table in self.db.tables:
+            s += self.initial_string(table)
+            s += self.column_string(table)
+            s += self.constraint_string(table)
+            s += self.primary_key_string(table)
         return s
 
-    def table_loop(self):
-        for table in self.db.tables:
-            self.table_string += self.string_builder(table)
+    # add logic to govern the order of create tables (e.g. those with foreign keys are made last)
 
 
 c = CreateTable(CreateDataBase(get_json("..\json_tables\\test_table.json")).get_db())
 
-c.table_loop()
-
-print(c.table_string)
+print(c.string_builder())
--- tests/test_create_tables.py
+++ tests/test_create_tables.py
@@ -2,3 +2,13 @@ from SQLServerMocker.get_database import *
 from SQLServerMocker.create_tables import *
 
 
+def test_initial_string():
+    pass
+
+def test_column_string():
+    pass
+
+def test_constraint_string():
+    pass
+
+def string_builder():
