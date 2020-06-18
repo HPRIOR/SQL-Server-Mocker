7e10531d962ec87ba4af53d4f721c5c617c38a35
HPRIOR <hprior02@dcs.bbk.ac.uk>
2020-06-18 21:07:12 +0100

    merging populate tables

--- SQLServerMocker/populate_tables.py
+++ SQLServerMocker/populate_tables.py
@@ -6,19 +6,18 @@ class PopulateTable:
     """
     generates string to populate table
     """
-    def __init__(self, db_name: str, table: Table, num_rows: int, generators: [Generator]):
+    def __init__(self, db_name: str, table: Table, num_rows: int):
         self.table = table
         self.num_rows = num_rows
-        self.generators = generators
         self.db_name = db_name
 
-    def string_builder(self):
+    def string_builder(self, generators: [Generator]):
         s = ""
         s += f"INSERT into {self.db_name}.dbo.{self.table.name} ({', '.join([col.name for col in self.table.columns])})\n"
         s += "VALUES\n"
         for i in range(self.num_rows):
             s += "("
-            for generator in self.generators:
+            for generator in generators:
                 s += generator.next()
             if i < self.num_rows:
                 s += "), \n"
@@ -26,6 +25,7 @@ class PopulateTable:
                 s += ") \n"
 
 
+# merge these two no point in making them seperate
 
 
 class Controller:
@@ -33,9 +33,10 @@ class Controller:
     Coordinates generation of table values
     checks table for fk/pk values, determines which generator is called by PopulateTable
     """
-    def __init__(self, db: DataBase, nums_rows: [int]):
+    def __init__(self, db: DataBase, nums_rows: [int], generators: [Generator]):
         self.db = db
         self.nums_rows = nums_rows
+        self.generators = generators
 
     def has_fk(self, table: Table) -> object:
         has_fk = False
