29a440a9e0b2adec10a7395837d831ac329161a3
HPRIOR <hprior02@dcs.bbk.ac.uk>
2020-06-23 16:39:59 +0100

    added tests for str_to_db_info.py

--- SQLServerMocker/str_to_db_info.py
+++ SQLServerMocker/str_to_db_info.py
@@ -3,15 +3,18 @@ def table_strings(inp: str) -> [str]:
 
 
 def table_dict(t_str: str) -> dict:
-    return {t_str.split(" ")[0].strip(): [s.strip() for s in t_str[t_str.find("(") + 1: t_str[1].find(")")].split(", ")]}
+    return {
+        t_str.split(" ")[0].strip(): [s.strip() for s in t_str[t_str.find("(") + 1: t_str[1].find(")")].split(", ")]
+    }
 
-def table_list(dicts: [dict]) -> list:
+
+def table_list(inp_str: str, t_dict=table_dict, t_string=table_strings) -> list:
+    return [t_dict(t_str) for t_str in t_string(inp_str)]
 
 
+t_s = "LOCATION (LOC_ID, LOC_NAME, LOC_CITY)" \
+      "\nCAR (CAR_ID, CAR_CATEGORY, CAR_MAKE, CAR_MODEL, LOC_ID)" \
+      "\nCUSTOMER (CUST_ID, CUST_NAME, CUST_CONTACT_NO)" \
+      "\nCAR_HIRE (HIRE_ID, CAR_ID, CUST_ID, START_DATE, START_MILEAGE, END_DATE, END_MILEAGE)"
 
-t_str = table_strings("LOCATION (LOC_ID, LOC_NAME, LOC_CITY)"
-                      "\nCAR (CAR_ID, CAR_CATEGORY, CAR_MAKE, CAR_MODEL, LOC_ID)"
-                      "\nCUSTOMER (CUST_ID, CUST_NAME, CUST_CONTACT_NO)"
-                      "\nCAR_HIRE (HIRE_ID, CAR_ID, CUST_ID, START_DATE, START_MILEAGE, END_DATE, END_MILEAGE)")
-
-print(table_dict(t_str[0]))
+print(table_list(t_s))
--- tests/test_string_to_db.py
+++ tests/test_string_to_db.py
@@ -1,17 +1,29 @@
 import pytest
 from SQLServerMocker.str_to_db_info import *
 
-
+s = "LOCATION (LOC_ID, LOC_NAME, LOC_CITY)" \
+    "\nCAR (CAR_ID, CAR_CATEGORY, CAR_MAKE, CAR_MODEL, LOC_ID)" \
+    "\nCUSTOMER (CUST_ID, CUST_NAME, CUST_CONTACT_NO)" \
+    "\nCAR_HIRE (HIRE_ID, CAR_ID, CUST_ID, START_DATE, START_MILEAGE, END_DATE, END_MILEAGE)"
 
 
 def test_table_strings():
-    assert table_strings("LOCATION (LOC_ID, LOC_NAME, LOC_CITY)"
-                             "\nCAR (CAR_ID, CAR_CATEGORY, CAR_MAKE, CAR_MODEL, LOC_ID)"
-                             "\nCUSTOMER (CUST_ID, CUST_NAME, CUST_CONTACT_NO)"
-                             "\nCAR_HIRE (HIRE_ID, CAR_ID, CUST_ID, START_DATE, START_MILEAGE, END_DATE, END_MILEAGE)") == \
-        [
-            "LOCATION (LOC_ID, LOC_NAME, LOC_CITY)",
-            "CAR (CAR_ID, CAR_CATEGORY, CAR_MAKE, CAR_MODEL, LOC_ID)",
-            "CUSTOMER (CUST_ID, CUST_NAME, CUST_CONTACT_NO)",
-            "CAR_HIRE (HIRE_ID, CAR_ID, CUST_ID, START_DATE, START_MILEAGE, END_DATE, END_MILEAGE)"
-        ]
+    assert table_strings(s) == \
+           [
+               "LOCATION (LOC_ID, LOC_NAME, LOC_CITY)",
+               "CAR (CAR_ID, CAR_CATEGORY, CAR_MAKE, CAR_MODEL, LOC_ID)",
+               "CUSTOMER (CUST_ID, CUST_NAME, CUST_CONTACT_NO)",
+               "CAR_HIRE (HIRE_ID, CAR_ID, CUST_ID, START_DATE, START_MILEAGE, END_DATE, END_MILEAGE)"
+           ]
+
+
+def test_table_dict():
+    assert table_dict(table_strings(s)[0]) == {"LOCATION": ["LOC_ID", "LOC_NAME", "LOC_CITY"]}
+
+
+def test_table_list():
+    assert table_list(s) == [{"LOCATION": ["LOC_ID", "LOC_NAME", "LOC_CITY"]},
+                             {"CAR": ["CAR_ID", "CAR_CATEGORY", "CAR_MAKE", "CAR_MODEL", "LOC_ID"]},
+                             {"CUSTOMER": ["CUST_ID", "CUST_NAME", "CUST_CONTACT_NO"]},
+                             {"CAR_HIRE": ["HIRE_ID", "CAR_ID", "CUST_ID", "START_DATE", "START_MILEAGE", "END_DATE",
+                                           "END_MILEAGE"]}]
