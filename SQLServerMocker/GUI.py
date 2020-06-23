4aaea75f8b7939651d0242efa606cbb111834b0c
HPRIOR <hprior02@dcs.bbk.ac.uk>
2020-06-23 19:26:30 +0100

    building main application

--- SQLServerMocker/GUI.py
+++ SQLServerMocker/GUI.py
@@ -1,3 +1,8 @@
-from tkinter import ttk
+import tkinter as tk
+
+class Application(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
 
 
